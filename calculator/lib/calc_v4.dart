70c76b20148232bc210be07ffa6382ad10677ded
donghwan.yu <koreacio@gmail.com>
2019-08-11 11:24:38 +0900

    [calculator] fix versions

--- calculator/lib/calc_v4.dart
+++ calculator/lib/calc_v4.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: true,
       home: new MyHomePage(title: 'Calculator'),
     );
   }
